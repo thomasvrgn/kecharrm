2a36affed3a64d2200d1fe8e3618765a76ea6271
NessMC <contact@nessmc.fr>
2020-11-21 22:03:20 +0100

    🔥 Default code removed from index page.

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,7 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        kecharrm
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <main>
+    <!-- Code -->
+  </main>
 </template>
 
 <script lang="ts">
