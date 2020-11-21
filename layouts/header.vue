cec781c4d12865a4c96f7a4b4536934aaab92e1b
NessMC <contact@nessmc.fr>
2020-11-21 22:23:22 +0100

    💄 Basic header UI added.

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,5 +1,21 @@
 <template>
-  <header>
-    <!-- Code -->
+  <header class="bg-indigo-900 relative py-8">
+    <span class="absolute inset-0 h-full w-full bg-black bg-opacity-50 z-0"></span>
+    <div class="container z-10 relative mx-auto">
+      <h1 class="uppercase text-white text-4xl font-bold">
+        Kecharrm
+      </h1>
+      <p class="text-white text-opacity-90 w-1/4 my-2 mb-8">
+        Kecharrm is a multi-domain community built after a server gathering.
+      </p>
+      <div class="container flex flex-row space-x-2">
+        <button class="bg-white text-indigo-800 font-medium py-1 px-4">
+          Découvrir
+        </button>
+        <button class="text-white py-1 px-4 focus:outline-none">
+          Rejoindre le discord
+        </button>
+      </div>
+    </div>
   </header>
 </template>
