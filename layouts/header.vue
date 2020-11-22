93dfec29082766dbc4beb7339b7aed66988a5abe
NessMC <contact@nessmc.fr>
2020-11-22 12:01:25 +0100

    💄 Padding vertical header updated.

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,11 +1,11 @@
 <template>
-  <header class="bg-indigo-900 relative py-8">
+  <header class="bg-indigo-900 relative py-32">
     <span class="absolute inset-0 h-full w-full bg-black bg-opacity-50 z-0"></span>
-    <div class="container z-10 relative mx-auto">
+    <div class="container z-10 relative mx-auto xl:w-1/2">
       <h1 class="uppercase text-white text-4xl font-bold">
         Kecharrm
       </h1>
-      <p class="text-white text-opacity-90 w-1/4 my-2 mb-8">
+      <p class="text-white text-opacity-90 w-1/3 my-2 mb-8">
         Kecharrm is a multi-domain community built after a server gathering.
       </p>
       <div class="container flex flex-row space-x-2">
