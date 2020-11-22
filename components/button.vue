6917f9c04676565a4904e5447ec23caa01d88d23
NessMC <contact@nessmc.fr>
2020-11-22 12:01:05 +0100

    💫 Button component animations added.

--- components/button.vue
+++ components/button.vue
@@ -1,7 +1,7 @@
 <template>
   <button
-    :class="{'bg-white text-indigo-800 font-medium': isDark(), 'bg-indigo-800 text-white font-medium': !isDark()}"
-    class="py-1 px-4"
+    :class="{'bg-white text-indigo-800 font-medium hover:bg-indigo-50 hover:text-indigo-900': isDark(), 'bg-indigo-800 text-white font-medium hover:bg-indigo-900': !isDark()}"
+    class="py-1 px-4 transition-colors duration-200"
   >
     <slot />
   </button>
