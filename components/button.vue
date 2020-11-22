d0eeea6ff09b76ae77f2cefef0efd1500ffe27b4
NessMC <contact@nessmc.fr>
2020-11-22 12:11:32 +0100

    ✨ Button component outline property added.

--- components/button.vue
+++ components/button.vue
@@ -1,10 +1,18 @@
 <template>
-  <button
-    :class="{'bg-white text-indigo-800 font-medium hover:bg-indigo-50 hover:text-indigo-900': isDark(), 'bg-indigo-800 text-white font-medium hover:bg-indigo-900': !isDark()}"
-    class="py-1 px-4 transition-colors duration-200"
-  >
-    <slot />
-  </button>
+  <div>
+    <button v-if="type === 'dark' && !outline" class="bg-white text-indigo-800 font-medium hover:bg-indigo-50 hover:text-indigo-900 py-1 px-2 border border-transparent">
+      <slot />
+    </button>
+    <button v-else-if="type === 'light' && !outline" class="bg-indigo-800 text-white font-medium py-1 px-2 hover:bg-indigo-900 border border-transparent">
+      <slot />
+    </button>
+    <button v-else-if="type === 'light' && outline" class="text-indigo-800 py-1 px-2 border border-indigo-800">
+      <slot />
+    </button>
+    <button v-else class="text-white py-1 px-2 border border-white">
+      <slot />
+    </button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -18,6 +26,11 @@ export default Vue.extend({
       type: String,
       required: true,
     } as PropOptions<Buttons>,
+    outline : {
+      type: Boolean,
+      required: false,
+      default: false,
+    } as PropOptions<Boolean>,
   },
   methods: {
     isDark(): Boolean {
